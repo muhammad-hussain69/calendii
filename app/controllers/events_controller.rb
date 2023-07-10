af3b92e9cb78a6ad4b94f252b95e7c011574227f
Muhammad Hussain <muhammad.hussain@engin.tech>
2023-07-10 19:06:10 +0500

    Updated

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,9 +25,13 @@ class EventsController < ApplicationController
     client = Google::Apis::CalendarV3::CalendarService.new
     client.authorization = credentials
 
-    @events = client.list_events('primary')
+    if client
+      @events = client.list_events('primary')
+      render 'events/view'
+    else
+      redirect_to new_user_session_path
+    end
 
-    render 'events/view'
   end
 
 end
